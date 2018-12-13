5b9e3962e1e5e6de134183a833a63e2d654caf95
DESKTOP-BAOA07E\dueho <duehoa1211@gmail.com>
2018-12-13 19:47:52 +0700

    New commit

--- Source/Application/Application/Controllers/CartController.cs
+++ Source/Application/Application/Controllers/CartController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Application.Models;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,28 +13,130 @@ namespace Application.Controllers
 {
     public class CartController : Controller
     {
+        IDbConnection connection;
+        public CartController()
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString);
+        }
         // GET: Cart
 
         // TODO: Thêm xóa sửa session đơn hàng
+        [HttpGet]
         public ActionResult Index()
         {
-            // TODO: 
-            return View();
+            var model = new CART();
+            model = Session["Cart"] as CART;
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddItem(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                using (connection)
+                {
+                    var product = connection.QueryFirstOrDefault<POST>(string.Format(@"
+                            SELECT [ID], [TITLE], [PRICE]
+                            FROM [dbo].[POST]
+                            WHERE [ID] = {0}
+                ", id));
+                    if (product != null)
+                    {
+                        var detail = new CART_DETAIL();
+                        detail.Product = product;
+                        detail.PRODUCT_ID = product.ID;
+                        var cart = Session["Cart"] as CART ?? new CART();
+                        cart.CartDetail.Add(detail);
+                        Session["Cart"] = cart;
+
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteItem(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var cart = Session["Cart"] as CART;
+                var result = cart.CartDetail.FirstOrDefault(z => z.PRODUCT_ID == id);
+                if (result != null)
+                {
+                    cart.CartDetail.Remove(result);
+                }
+                Session["Cart"] = cart;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateQuantity(int id, int quantity)
+        {
+            if (ModelState.IsValid)
+            {
+                var cart = Session["Cart"] as CART;
+                var result = cart.CartDetail.FirstOrDefault(z => z.PRODUCT_ID == id);
+                result.QUANTITY += quantity;
+                Session["Cart"] = cart;
+            }
+            return RedirectToAction("Index");
         }
 
         [HttpGet]
         public ActionResult CheckOutView()
         {
-            // TODO: View của thanh toán đơn hàng theo class CART
-            //       
-            return View();
+            var cart = Session["Cart"] as CART;
+            return View(cart);
         }
 
         [HttpPost]
-        public ActionResult Checkout()
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkout(CART model)
         {
-            // TODO: Thanh toán đơn hàng aka thêm vào database 
-            return View();
+            var cart = Session["Cart"] as CART;
+            if (ModelState.IsValid)
+            {
+                using (connection)
+                {
+
+                    model.CartDetail = cart.CartDetail;
+                    connection.Execute(string.Format(@"
+                            INSERT INTO [dbo].[CART]
+                                   ([INFOS]
+                                   ,[CUSTOMER]
+                                   ,[ADDRESS]
+                                   ,[PHONENUMBER]
+                                   ,[EMAIL])
+                             VALUES
+                                   ({0}
+                                   ,{1}
+                                   ,{2}
+                                   ,{3}
+                                   ,{4})
+                    ", model.INFOS, model.CUSTOMER, model.ADDRESS,
+                    model.PHONENUMBER, model.EMAIL));
+                    connection.Execute(string.Format(@"
+                            INSERT INTO [dbo].[CART_DETAIL]
+                                   ([BILL_ID]
+                                   ,[PRODUCT_ID]
+                                   ,[QUANTITY])
+                             VALUES
+                                   (@BILL_ID
+                                   ,@PRODUCT_ID
+                                   ,@QUANTITY
+                        
+                    "), model.CartDetail);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View("CheckOutView", model);
         }
     }
 }
--- Source/Application/Application/Models/CART_DETAIL.cs
+++ Source/Application/Application/Models/CART_DETAIL.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Application.Models
 {
@@ -9,11 +11,16 @@ namespace Application.Models
     {
         public int BILL_ID { get; set; }
 
+        [Required]
+        [Display(Name ="Sản phẩm")]
         public int? PRODUCT_ID { get; set; }
 
+        [Required]
+        [Display(Name ="Số lượng")]
         public int? QUANTITY { get; set; }
 
         public POST Product { get; set; }
 
+        public SelectList ProductList { get; set; }
     }
 }
